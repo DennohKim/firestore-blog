add2158d6a677173994cd04991ff8d89778c8023
“dennohkim” <kimathidennis38@gmail.com>
2023-01-15 18:50:40 +0300

    feat: google auth

--- pages/enter.js
+++ pages/enter.js
@@ -1,8 +1,45 @@
-export default function EnterPage({}){
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleAuthProvider } from "../lib/firebase"
+
+export default function Enter(props){
+
+    const user = null;
+    const username = null;
+    //1. User signed out <SignInButton/>
+    //1. User signed in, but missing username <UsernameForm/>
+    //1. User signed in, has username <SignOutButton/>
     return (
         <main>
-            <h1>Sign up</h1>
+            {user ? !username ? <UsernameForm /> : <SignOutButton/> : <SignInButton/>}
 
         </main>
     )
 }
+
+//Sign in with google
+function SignInButton(){
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, googleAuthProvider);
+    }
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} />
+            Sign in with Google
+        </button>
+    )
+
+}
+
+
+//Sign out button
+function SignOutButton(){
+    return <button onClick={() => signOut(auth)}>Sign Out</button>
+
+}
+ 
+function UsernameForm(){
+
+
+}
+
+ 
