ee8c3643920ff157aeb68983689585c3346a2eaa
“dennohkim” <kimathidennis38@gmail.com>
2023-01-15 15:55:34 +0300

    feat: create navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const  user = null
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* User signed in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* User is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
