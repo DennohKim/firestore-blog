8288512e577fe5dd86d6dbe44f81208983b9de91
“dennohkim” <kimathidennis38@gmail.com>
2023-01-15 15:26:55 +0300

    feat: define routes

---
+++ pages/admin/index.js
@@ -0,0 +1,5 @@
+export default function AdminPostPage({}){
+    return (
+        <main></main>
+    )
+}
---
+++ pages/enter.js
@@ -0,0 +1,8 @@
+export default function EnterPage({}){
+    return (
+        <main>
+            <h1>Sign up</h1>
+
+        </main>
+    )
+}
